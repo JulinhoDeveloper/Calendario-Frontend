11b2d2bc271837b65cabefa76decb705111bf183
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-09-03 18:52:35 -0300

    personalizando o calendário

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,53 @@
 import React from 'react'
 
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+
+import { Navbar } from '../ui/Navbar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/pt';
+import { messages } from '../../helpers/calendar-messages-pt';
+moment.locale('pt');
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: 'oioioioi',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: 'green'
+}]
+
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+
+        return {
+            style
+        }
+    };
+
+
     return (
-        <div>
-            <h1>CalendarScreen</h1>
+        <div className="calendar-screen">
+           <Navbar />
+           <Calendar
+                localizer={ localizer }
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+               messages = {messages}
+               eventPropGetter={ eventStyleGetter }
+                />
+           
         </div>
     )
 }
---
+++ src/helpers/calendar-messages-pt.js
@@ -0,0 +1,15 @@
+export const messages = {
+    allDay: 'Todo  dia',
+    previous: '<',
+    next: '>',
+    today: 'Hoje',
+    month: 'Mes',
+    week: 'Semana',
+    day: 'Dia',
+    agenda: 'Agenda',
+    date: 'data',
+    time: 'Hora',
+    event: 'Evento',
+    noEventsInRange: 'Não há eventos nessa data',
+    showMore: total => `+ Ver mais (${total})`
+};
